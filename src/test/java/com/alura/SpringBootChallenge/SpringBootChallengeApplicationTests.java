4a1cb0ea01a7d09956f96885fe817fcf66f33a7e
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-10-28 20:13:07 -0500

    feat: Inicializar proyecto "Spring Boot Challenge"
    - Configuración inicial del proyecto utilizando Spring Boot.
    - Agregado archivo `pom.xml` con dependencias necesarias para Spring.
    - Estructura de directorios creada: `src/main/java` y `src/main/resources`.
    - Clase principal generada con la anotación `@SpringBootChallengeApplication` para el inicio de la aplicación.

---
+++ src/test/java/com/alura/SpringBootChallenge/SpringBootChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.SpringBootChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
