fe4f007ad24a61d6ca1c14f710cd57f1b607a220
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-10-30 12:14:17 -0500

    feat: Implementar método de consulta de datos en clase Principal
    - Agrega la clase `Principal` con el método `muestraElMenu()` que realiza una solicitud de datos a una API externa usando `ConsumoAPI`.
    - Obtiene un JSON desde la URL base de `https://gutendex.com/books/` y lo imprime en la consola.
    - Configura la estructura inicial para la obtención y visualización de datos en la aplicación.

---
+++ src/main/java/com/alura/SpringBootChallenge/principal/Principal.java
@@ -0,0 +1,22 @@
+package com.alura.SpringBootChallenge.principal;
+
+import com.alura.SpringBootChallenge.service.ConsumoAPI;
+import com.alura.SpringBootChallenge.service.ConvierteDatos;
+
+public class Principal {
+    // URL base para realizar la solicitud a la API externa
+    private static final String URL_BASE = "https://gutendex.com/books/";
+
+    // Instancia de ConsumoAPI para manejar las solicitudes a la API
+    private ConsumoAPI consumoAPI = new ConsumoAPI();
+    // Instancia de ConvierteDatos para manejar la conversión de datos
+    private ConvierteDatos convierteDatos = new ConvierteDatos();
+
+    // Método que muestra el menú principal de la aplicación
+    public void muestraElMenu() {
+        // Realiza una solicitud a la API y obtiene el JSON de respuesta
+        var json = consumoAPI.obtenerDatos(URL_BASE);
+        // Imprime el JSON obtenido en la consola
+        System.out.println(json);
+    }
+}
