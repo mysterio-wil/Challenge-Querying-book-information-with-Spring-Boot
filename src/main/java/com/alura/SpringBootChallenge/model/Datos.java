dcaf662fa9383f7d14793fa1308b98c06ba74397
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-10-30 13:39:22 -0500

    fix: Corregir la declaración del record Datos
    - Cambiar la declaración de 'public record Datos<DatosLibros>(){}' a 'record Datos(){}' para eliminar la generics innecesaria.

--- src/main/java/com/alura/SpringBootChallenge/model/Datos.java
+++ src/main/java/com/alura/SpringBootChallenge/model/Datos.java
@@ -8,7 +8,7 @@ import java.util.List;
 // Esta clase representa la estructura de datos para almacenar los resultados de la API.
 // Se utiliza la anotación JsonIgnoreProperties para ignorar cualquier propiedad desconocida en la respuesta JSON.
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record Datos<DatosLibros>(
+public record Datos(
         // Asocia el campo 'results' del JSON con el campo 'resultados' en la clase
         @JsonAlias("results") List<DatosLibros> resultados
 ) {
