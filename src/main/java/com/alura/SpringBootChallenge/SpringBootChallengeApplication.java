6398d5a691b4f0976b14f43b6444c24f4e012bdb
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-10-30 12:15:22 -0500

    feat: Configurar clase principal para iniciar el menú de la aplicación
    - Implementa CommandLineRunner en `SpringBootChallengeApplication` para ejecutar el método `muestraElMenu()` de la clase `Principal` al iniciar la aplicación.
    - Permite la ejecución automática del menú principal una vez que la aplicación Spring Boot se ejecuta.

--- src/main/java/com/alura/SpringBootChallenge/SpringBootChallengeApplication.java
+++ src/main/java/com/alura/SpringBootChallenge/SpringBootChallengeApplication.java
@@ -1,13 +1,20 @@
 package com.alura.SpringBootChallenge;
 
+import com.alura.SpringBootChallenge.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootChallengeApplication {
+public class SpringBootChallengeApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootChallengeApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.muestraElMenu();
+	}
 }
